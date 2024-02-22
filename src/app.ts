92bb7a0ca28520041c486c9b77d8a81472094b13
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-23 03:15:15 +0600

    feat: add create-user business logic

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import userRoute from "./app/modules/users/users.route";
 
 const app: Application = express();
 app.use(cors());
@@ -7,6 +8,9 @@ app.use(cors());
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//app route
+app.use("/api/v1/users/", userRoute);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello World!");
