c9a7650b20e6d4f623b7987884a05ae9a6c9c734
nayeem <nayeemuddin.bd100@gmail.com>
2024-03-07 10:49:01 +0600

    feat: check same year semester exist or not

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,6 @@
+import { StatusCodes } from "http-status-codes";
 import { Schema, model } from "mongoose";
+import ApiError from "../../../errors/ApiError";
 import {
   academicSemesterCode,
   academicSemesterMonth,
@@ -38,6 +40,18 @@ const AcademicSemesterSchema = new Schema<IAcademicSemester>(
   },
   { timestamps: true }
 );
+
+AcademicSemesterSchema.pre("save", async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(StatusCodes.CONFLICT, "Academic semester already exist");
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   "AcademicSemester",
