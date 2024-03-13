cd2400adfe4d50e59a00c36f5a6a565632af06b5
nayeem <nayeemuddin.bd100@gmail.com>
2024-03-13 12:13:00 +0600

    feat: pagination and sorting add

--- src/app/interfaces/common.ts
+++ src/app/interfaces/common.ts
@@ -1,5 +1,14 @@
 import { IGenericErrorMsg } from "./error";
 
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
+
 export type IGenericMsgResponse = {
   statusCode: number;
   message: string;
