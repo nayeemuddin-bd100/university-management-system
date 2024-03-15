f7ad7b0c9be0e7a3aae947e0845bf0c184315309
nayeem <nayeemuddin.bd100@gmail.com>
2024-03-15 12:11:35 +0600

    feat:remove next middleware

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,23 +1,19 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { usersService } from "./user.service";
 
-const createUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { user } = req.body;
-    const result = await usersService.createUser(user);
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { user } = req.body;
+  const result = await usersService.createUser(user);
 
-    sendResponse(res, {
-      statusCode: StatusCodes.OK,
-      success: true,
-      message: "User created successfully",
-      data: result,
-    });
-
-    next();
-  }
-);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "User created successfully",
+    data: result,
+  });
+});
 
 export const userController = { createUser };
