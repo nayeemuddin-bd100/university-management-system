abb65716778fbfeaba7c3cff93ca37e84bff6739
nayeem <nayeemuddin.bd100@gmail.com>
2024-03-17 03:31:04 +0600

    fix: update validation method

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,11 +1,10 @@
 import { z } from "zod";
 const createUserZodSchema = z.object({
   body: z.object({
-    user: z.object({
-      role: z.string({
-        required_error: "Role is required",
-      }),
+    role: z.string({
+      required_error: "Role is required",
     }),
+
     password: z.string().optional(),
   }),
 });
