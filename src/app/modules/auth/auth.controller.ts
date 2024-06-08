53d2c50dafba54d2b2ab2cfaaca879831385b2eb
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-08 23:47:54 +0600

    feat: set refresh token in cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import config from "../../../config";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
+import { ILoginResponse } from "./auth.interface";
 import { authService } from "./auth.service";
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
@@ -9,11 +11,20 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 
   const result = await authService.loginUser(loginData);
 
-  sendResponse(res, {
+  const { refreshToken, ...rest } = result;
+
+  const cookiesOptions = {
+    secure: config.env === "production",
+    httpOnly: true,
+  };
+
+  res.cookie("refreshToken", refreshToken, cookiesOptions);
+
+  sendResponse<ILoginResponse>(res, {
     statusCode: StatusCodes.OK,
     success: true,
     message: "User Login Successfully",
-    data: result,
+    data: rest,
   });
 });
 
