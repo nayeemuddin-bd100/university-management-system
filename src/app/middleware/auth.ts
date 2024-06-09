5f6a3ceb5fb1562617893b41757bada6ed11cb1a
nayeem <nayeemuddin.bd100@gmail.com>
2024-06-09 23:05:03 +0600

    feat: add authgurd to control role based request

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { Secret } from "jsonwebtoken";
+import config from "../../config";
+import ApiError from "../../errors/ApiError";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Get Token
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(
+          StatusCodes.UNAUTHORIZED,
+          "Authorization token required"
+        );
+      }
+
+      // verify token
+      let verifiedUser = null;
+      try {
+        verifiedUser = jwtHelpers.verifyToken(
+          token,
+          config.jwt.refresh_secret as Secret
+        );
+      } catch (err) {
+        throw new ApiError(StatusCodes.FORBIDDEN, "Invalid  token");
+      }
+
+      req.user = verifiedUser; //role,userId
+
+      //check user role
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(StatusCodes.FORBIDDEN, "Unauthorized access");
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default auth;
